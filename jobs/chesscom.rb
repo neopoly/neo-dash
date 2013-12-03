15d574b4a26f9eb7fbe94ddd3ca137a50223a049
Peter Suschlik <ps@neopoly.de>
2013-12-03 12:51:04 +0100

    Add chess.com job parsing current game

---
+++ jobs/chesscom.rb
@@ -0,0 +1,53 @@
+require 'open-uri'
+require 'uri'
+
+CHESSCOM_EVERY = ENV['CHESSCOM_EVERY'] || "5m"
+CHESSCOM_USERNAME = ENV['CHESSCOM_USERNAME']
+
+abort "Need CHESSCOM_USERNAME to be set" unless CHESSCOM_USERNAME
+
+class Chesscom
+  VIEW_MEMBER = "http://www.chess.com/members/view/%{username}"
+  FEN_JS = %r{acd_register\("\d+", "([^"]+)"}
+  FEN_BOARD = "http://www.chess.com/diagram?fen=%{fen}"
+
+  def self.extract_fens(username)
+    content = open(VIEW_MEMBER % { :username => username }).read
+    content.scan(FEN_JS).map(&:first)
+  end
+
+  def self.board_url(fen)
+    fen = URI.encode(fen)
+    FEN_BOARD % { :fen => fen }
+  end
+
+  def self.run(username)
+    fens = extract_fens(username)
+    if fens.any?
+    else
+
+    end
+  end
+
+  def initialize(username, sender)
+    @username = username
+    @sender   = sender
+  end
+
+  def run
+    fens = self.class.extract_fens(@username)
+    if fens.any?
+      board_url = self.class.board_url(fens.first)
+      send_event(board_url)
+    end
+  end
+
+  def send_event(board_url)
+    @sender.send_event 'chesscom',
+      url: board_url
+  end
+end
+
+SCHEDULER.every CHESSCOM_EVERY, :first_in => 0 do
+  Chesscom.new(CHESSCOM_USERNAME, SENDER).run
+end
