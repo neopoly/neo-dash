e9d4a76f6df2af439804ba1f96c3b78e4391d697
Peter Suschlik <ps@neopoly.de>
2013-12-05 18:11:32 +0100

    Add ship job

---
+++ jobs/ship.rb
@@ -0,0 +1,9 @@
+SHIP_EVERY = ENV['SHIP_EVERY'] || "1m"
+SHIP_URL = ENV['SHIP_URL']
+
+abort "Need SHIP_URL to be set" unless SHIP_URL
+
+SCHEDULER.every "1m", :first_in => 0 do
+  url = SHIP_URL % { :t => Time.now.to_i }
+  SENDER.send_event 'ship', url: url
+end
