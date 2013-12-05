c7527450f715b0c27ce8b6da4b1fc83c9754179b
Peter Suschlik <ps@neopoly.de>
2013-12-05 18:14:17 +0100

    Parameterize refresh interval for ship job

--- jobs/ship.rb
+++ jobs/ship.rb
@@ -3,7 +3,7 @@ SHIP_URL = ENV['SHIP_URL']
 
 abort "Need SHIP_URL to be set" unless SHIP_URL
 
-SCHEDULER.every "1m", :first_in => 0 do
+SCHEDULER.every SHIP_EVERY, :first_in => 0 do
   url = SHIP_URL % { :t => Time.now.to_i }
   SENDER.send_event 'ship', url: url
 end
