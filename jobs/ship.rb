3a2aa9a2ae3873fdd4bb1edc7d541a49614b645a
Peter Suschlik <ps@neopoly.de>
2013-12-17 16:29:29 +0100

    Allow SHIP_URL to be a comma-separated list of images

--- jobs/ship.rb
+++ jobs/ship.rb
@@ -1,9 +1,9 @@
 SHIP_EVERY = ENV['SHIP_EVERY'] || "1m"
-SHIP_URL = ENV['SHIP_URL']
+SHIP_URL = ENV['SHIP_URL'].to_s.split(",")
 
-abort "Need SHIP_URL to be set" unless SHIP_URL
+abort "Need SHIP_URL to be set" if SHIP_URL.empty?
 
 SCHEDULER.every SHIP_EVERY, :first_in => 0 do
-  url = SHIP_URL % { :t => Time.now.to_i }
+  url = SHIP_URL.shuffle.first
   SENDER.send_event 'ship', url: url
 end
