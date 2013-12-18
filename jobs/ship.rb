453c269e8c0bff69341f8ba3297e4b238ddec348
Peter Suschlik <ps@neopoly.de>
2013-12-18 10:49:18 +0100

    Do not run ship job if SHIP_URL is missing

--- jobs/ship.rb
+++ jobs/ship.rb
@@ -1,9 +1,9 @@
 SHIP_EVERY = ENV['SHIP_EVERY'] || "1m"
 SHIP_URL = ENV['SHIP_URL'].to_s.split(",")
 
-abort "Need SHIP_URL to be set" if SHIP_URL.empty?
-
-SCHEDULER.every SHIP_EVERY, :first_in => 0 do
-  url = SHIP_URL.shuffle.first
-  SENDER.send_event 'ship', url: url
+unless SHIP_URL.empty?
+  SCHEDULER.every SHIP_EVERY, :first_in => 0 do
+    url = SHIP_URL.shuffle.first
+    SENDER.send_event 'ship', url: url
+  end
 end
