1a5713edb69ca12baf9424669492ee6d23e4b925
Peter Suschlik <ps@neopoly.de>
2013-12-18 10:51:29 +0100

    Add nichtlustig.de job

---
+++ jobs/nichtlustig.rb
@@ -0,0 +1,18 @@
+
+NICHTLUSTIG_EVERY = ENV['NICHTLUSTIG_EVERY'] || "5m"
+NICHTLUSTIG_OVERVIEW_URL = ENV['NICHTLUSTIG_OVERVIEW_URL']
+NICHTLUSTIG_IMAGE_URL = ENV['NICHTLUSTIG_IMAGE_URL'] || "http://static.nichtlustig.de/comics/full/%s.jpg"
+
+if NICHTLUSTIG_OVERVIEW_URL
+  require 'open-uri'
+
+  overview = open(NICHTLUSTIG_OVERVIEW_URL).read.scan(%r{href="/toondb/(\d+)\.html"}i)
+  ids = overview.map { |x| x[0] }
+
+  SCHEDULER.every SHIP_EVERY, :first_in => 0 do
+    id = ids.sample
+    url = NICHTLUSTIG_IMAGE_URL % id
+    p url
+    SENDER.send_event 'nichtlustig', url: url
+  end
+end
