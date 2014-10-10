5e3c20513e630fe96710a86cd526475ec93da877
Peter Suschlik <ps@neopoly.de>
2014-10-10 13:06:29 +0200

    Inject only the body into DashboardNotepad

--- jobs/dashboad_notepad.rb
+++ jobs/dashboad_notepad.rb
@@ -7,7 +7,7 @@ unless DASHBOARD_NOTEPAD_URL.empty?
   SCHEDULER.every DASHBOARD_NOTEPAD_EVERY, :first_in => 0 do
     url = DASHBOARD_NOTEPAD_URL.shuffle.first
 
-    content = Curl::Easy.perform(url).body_str
+    content = Curl::Easy.perform(url).body_str[%r{<body>(.*)</body>}m, 1]
 
     SENDER.send_event 'dashboard_notepad', content: content
   end
